4babf6ed165686f26c993a8548849e4b82e0320c
Akshay Thalinja <154006952+akstspace@users.noreply.github.com>
2026-01-19 02:12:41 +0530

    Move tool installations to Dockerfile
    Shifted installation of powerlevel10k, volta, and pyenv from post-create.sh to the Dockerfile. Also added required development libraries to the Dockerfile for improved environment setup and reproducibility.

--- dev-environment/scripts/post-create.sh
+++ dev-environment/scripts/post-create.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-# Install volta
-curl https://get.volta.sh | bash
-
-# Install pyenv
-curl https://pyenv.run | bash
-
 # Copy dotfiles
 cp .devcontainer/dotfiles/.zshrc ~
 cp .devcontainer/dotfiles/.p10k.zsh ~
